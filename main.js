e8b5e088c1bd5e3a8c09b0e4c1546e366df5909d
abd360superman <78216399+abd360Superman@users.noreply.github.com>
2021-08-14 19:19:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const data = [
     {
         name: 'Midnight Celebration!',
         details: ['Hukka', 'Chocolate mousse bowl', 'Sushi cake', 'Rose wine', 'Cocktails and mocktails', 'Tequilla shots', 'Tit bits', 'Dimsums'],
-        startTime: [2021, 7, 14, 22, 30]
+        startTime: [2021, 7, 14, 00, 00]
     },
     {
         name: 'Bed Tea',
